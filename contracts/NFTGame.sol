01c89620455762e6489b40580e64f64f0dae5253
Pervez Shoaib <50441829+PervezSH@users.noreply.github.com>
2021-12-19 16:48:34 +0530

    feat (contract) : set up data for character NFTs✨

--- contracts/NFTGame.sol
+++ contracts/NFTGame.sol
@@ -4,7 +4,40 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract NFTGame {
-    constructor() {
-        console.log("I'm a Game Contract:)");
+    // Character's attributes
+    struct CharacterAttributes {
+        uint characterIndex;
+        string name;
+        string imageURI;
+        uint hp;
+        uint maxHp;
+        uint attackDamage;
+    }
+    // Array that hold default data of characters
+    CharacterAttributes[] defaultCharacters;
+
+    // Pass data into the contract when it is created
+    constructor(
+        string[] memory characterNames,
+        string[] memory characterImageURIs,
+        uint[] memory characterHp,
+        uint[] memory characterAttackDmg
+    ) {
+        // Loop through all characters, and save their values in contract
+        for (uint i = 0; i < characterNames.length; i++) {
+            defaultCharacters.push(
+                CharacterAttributes({
+                    characterIndex : i,
+                    name : characterNames[i],
+                    imageURI : characterImageURIs[i],
+                    hp : characterHp[i],
+                    maxHp : characterHp[i],
+                    attackDamage : characterAttackDmg[i]
+                })
+            );
+
+            CharacterAttributes memory c = defaultCharacters[i];
+            console.log("Done Creating %s with HP %s, img URI %s", c.name, c.hp, c.imageURI);
+        }
     }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,6 +1,14 @@
 const main = async () => {
     const gameContractFactory = await hre.ethers.getContractFactory('NFTGame');
-    const gameContract = await gameContractFactory.deploy();
+    const gameContract = await gameContractFactory.deploy(
+        ["Todo", "Yuji", "Maki", "Gojo"],   // Names
+        ["https://i.imgur.com/oPB9pxw.gif",
+        "https://i.imgur.com/JUmSOnx.gif",
+        "https://i.imgur.com/IOsVeUv.gif",
+        "https://i.imgur.com/zvh4w0Q.gif"], // Images
+        [500, 300, 200, 1000],  // Hps
+        [100, 75, 50, 500]  // Attack Damage Values
+    );
     await gameContract.deployed();
 
     console.log("Game contrat deployed to : ", gameContract.address);
